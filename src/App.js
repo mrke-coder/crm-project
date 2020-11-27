f716fa0b6a5e8b355ba76df84d246d89b27398b7
Mr KE <ernestkouassi02@gmail.com>
2020-11-27 08:20:33 +0000

    INSTALLATION DU PLUGIN LAILWINDCSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <p>
+            <a href="https://tailwindcss.com"
+              className="text-cyan-600 hover:text-cyan-700">
+                Read the docs &rarr; 
+            </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
